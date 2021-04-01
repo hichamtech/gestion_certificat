18848cf563246c5b5b41ce9587f94d0ecd2e76c2
Hicham HASSANI <hicham.h@hiitconsulting.com>
2021-04-01 15:36:09 +0100

    init

---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,297 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtudiantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EtudiantRepository::class)
+ */
+class Etudiant
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $codeApogee;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $cne;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $cin;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateNaissance;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $villeNaissance;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $paysNaissance;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $sexe;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $anneePremiereInscription;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $anneePremiereInscriptionSup;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $anneePremiereInscriptionUniMaro;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $codeBac;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $serieBac;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Filiere::class, inversedBy="etudiants")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $filiere;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodeApogee(): ?string
+    {
+        return $this->codeApogee;
+    }
+
+    public function setCodeApogee(string $codeApogee): self
+    {
+        $this->codeApogee = $codeApogee;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getCne(): ?string
+    {
+        return $this->cne;
+    }
+
+    public function setCne(string $cne): self
+    {
+        $this->cne = $cne;
+
+        return $this;
+    }
+
+    public function getCin(): ?string
+    {
+        return $this->cin;
+    }
+
+    public function setCin(string $cin): self
+    {
+        $this->cin = $cin;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->dateNaissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $dateNaissance): self
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    public function getVilleNaissance(): ?string
+    {
+        return $this->villeNaissance;
+    }
+
+    public function setVilleNaissance(string $villeNaissance): self
+    {
+        $this->villeNaissance = $villeNaissance;
+
+        return $this;
+    }
+
+    public function getPaysNaissance(): ?string
+    {
+        return $this->paysNaissance;
+    }
+
+    public function setPaysNaissance(string $paysNaissance): self
+    {
+        $this->paysNaissance = $paysNaissance;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getAnneePremiereInscription(): ?string
+    {
+        return $this->anneePremiereInscription;
+    }
+
+    public function setAnneePremiereInscription(string $anneePremiereInscription): self
+    {
+        $this->anneePremiereInscription = $anneePremiereInscription;
+
+        return $this;
+    }
+
+    public function getAnneePremiereInscriptionSup(): ?string
+    {
+        return $this->anneePremiereInscriptionSup;
+    }
+
+    public function setAnneePremiereInscriptionSup(string $anneePremiereInscriptionSup): self
+    {
+        $this->anneePremiereInscriptionSup = $anneePremiereInscriptionSup;
+
+        return $this;
+    }
+
+    public function getAnneePremiereInscriptionUniMaro(): ?string
+    {
+        return $this->anneePremiereInscriptionUniMaro;
+    }
+
+    public function setAnneePremiereInscriptionUniMaro(string $anneePremiereInscriptionUniMaro): self
+    {
+        $this->anneePremiereInscriptionUniMaro = $anneePremiereInscriptionUniMaro;
+
+        return $this;
+    }
+
+    public function getCodeBac(): ?string
+    {
+        return $this->codeBac;
+    }
+
+    public function setCodeBac(string $codeBac): self
+    {
+        $this->codeBac = $codeBac;
+
+        return $this;
+    }
+
+    public function getSerieBac(): ?string
+    {
+        return $this->serieBac;
+    }
+
+    public function setSerieBac(string $serieBac): self
+    {
+        $this->serieBac = $serieBac;
+
+        return $this;
+    }
+
+    public function getFiliere(): ?Filiere
+    {
+        return $this->filiere;
+    }
+
+    public function setFiliere(?Filiere $filiere): self
+    {
+        $this->filiere = $filiere;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Filiere.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FiliereRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FiliereRepository::class)
+ */
+class Filiere
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Etudiant::class, mappedBy="filiere")
+     */
+    private $etudiants;
+
+    public function __construct()
+    {
+        $this->etudiants = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Etudiant[]
+     */
+    public function getEtudiants(): Collection
+    {
+        return $this->etudiants;
+    }
+
+    public function addEtudiant(Etudiant $etudiant): self
+    {
+        if (!$this->etudiants->contains($etudiant)) {
+            $this->etudiants[] = $etudiant;
+            $etudiant->setFiliere($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtudiant(Etudiant $etudiant): self
+    {
+        if ($this->etudiants->removeElement($etudiant)) {
+            // set the owning side to null (unless already changed)
+            if ($etudiant->getFiliere() === $this) {
+                $etudiant->setFiliere(null);
+            }
+        }
+
+        return $this;
+    }
+}
